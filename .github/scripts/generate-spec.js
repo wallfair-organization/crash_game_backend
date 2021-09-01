07310798d07c79781bc6921f238f6cb86b8251e2
Guilherme Mussi <gmussi@gmail.com>
2021-09-01 11:19:43 +0200

    Add CI integration

---
+++ .github/scripts/generate-spec.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const fs = require("fs");
+
+let yml = fs.readFileSync(path.resolve(__dirname, `spec-${process.env.stage}.yml`)).toString();
+console.log(
+    yml
+    .replace("$VERSION", process.env.version)
+    .replace("$JWT", process.env.JWT_KEY)
+    .replace("$POSTGRES_PASSWORD", process.env.POSTGRES_PASSWORD)
+    );
