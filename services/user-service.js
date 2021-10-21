1a4ca14970b0103df5536971d804756db5fc36cc
Adrian Szlagor <adrian@fx1.eu>
2021-10-21 11:26:22 +0200

    MVP2-619 - add missing services to handle total played award

---
+++ services/user-service.js
@@ -0,0 +1,67 @@
+const {WFAIR_REWARDS} = require('../utils/constants')
+const {getCasinoGamePlayCount} = require("./statistics-service");
+const { publishEvent, notificationEvents } = require('./notification-service')
+
+const { Erc20 } = require('@wallfair.io/smart_contract_mock');
+
+exports.mintUser = async (userId, amount) => {
+    const WFAIR = new Erc20('WFAIR');
+    if(amount) {
+        await WFAIR.mint(userId, BigInt(amount) * WFAIR.ONE).catch((err)=> {
+            console.error("mintUser err", err);
+        });
+    }
+};
+
+/***
+ * create USER_AWARD event in universalevents, add proper token amount based on `awardData.award` amount
+ * @param userId
+ * @returns {Promise<void>} undefined
+ */
+
+exports.createUserAwardEvent = async ({userId, awardData, broadcast = false}) => {
+    //add token amount for award during event creation
+    if(awardData?.award) {
+        await this.mintUser(userId, awardData.award).catch((err)=> {
+            console.error('award mintUser', err)
+        })
+    }
+
+    publishEvent(notificationEvents.EVENT_USER_AWARD, {
+        producer: 'user',
+        producerId: userId,
+        data: awardData,
+        broadcast
+    });
+}
+
+/***
+ * check total played games for user and save USER_AWARD event, after reaching each levels
+ * @param userId
+ * @param gameData
+ * @returns {Promise<void>} undefined
+ */
+exports.checkTotalGamesPlayedAward = async (userId, gameData) => {
+    const awardData = {
+        type: `TOTAL_ROSI_GAME_PLAYED_ABOVE_VALUE`,
+        ...gameData
+    };
+
+    const totalPlayed = await getCasinoGamePlayCount(userId, gameData.gameTypeId).catch((err)=> {
+        console.error('getCasinoGamePlayCount', err)
+    });
+
+    const total = awardData.total = totalPlayed || 0;
+    if(total === 5 || total === 20 || total === 40 || total === 60 || total === 100) {
+        awardData.award = WFAIR_REWARDS.totalGamesPlayed[total];
+        awardData.total = total;
+
+        //publish in universalevents collection and add tokens
+        await this.createUserAwardEvent({
+            userId,
+            awardData
+        }).catch((err)=> {
+            console.error('createUserAwardEvent', err)
+        })
+    }
+}
